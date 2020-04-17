e612863d97d49790a9ed3f9f9bd2d4f230c1d932
Joshua Haunty <jkh87440@ucmo.edu>
2020-04-16 19:32:05 -0500

    Logic created for MealPlanController, MealPlanService, and MealPlanRepository.

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,6 @@
 INSERT INTO `role`( `role`) VALUES ("ROLE_ADMIN");
 INSERT INTO `role`( `role`) VALUES ("ROLE_USER");
 
-INSERT INTO `user`( `enabled`, `password`, `username`) VALUES (true, "$2a$10$0Gdl4wkmMSDDMjMAh9/PI.MVydfp0izSJx1tWDX3AP39xme0j1yWu","admin");#admin
+-- INSERT INTO `user`( `enabled`, `password`, `username`) VALUES (true, "$2a$10$0Gdl4wkmMSDDMjMAh9/PI.MVydfp0izSJx1tWDX3AP39xme0j1yWu","admin");
 
 INSERT INTO `user_role`(`user_id`, `role_id`) VALUES (1,1);
