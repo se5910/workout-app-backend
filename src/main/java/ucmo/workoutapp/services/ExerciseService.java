e661f241bc8b6b307020785ea71401485df74de3
Joshua Haunty <jkh87440@ucmo.edu>
2020-04-29 16:00:45 -0500

    Added exception for trying to create more than one Exercise entity per ExerciseSlot

--- src/main/java/ucmo/workoutapp/services/ExerciseService.java
+++ src/main/java/ucmo/workoutapp/services/ExerciseService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import ucmo.workoutapp.entities.Exercise;
 import ucmo.workoutapp.entities.ExerciseSlot;
+import ucmo.workoutapp.exceptions.DuplicateExerciseCreationException;
 import ucmo.workoutapp.repositories.ExerciseRepository;
 import ucmo.workoutapp.repositories.ExerciseSlotRepository;
 
@@ -16,8 +17,15 @@ public class ExerciseService {
     private ExerciseSlotRepository exerciseSlotRepository;
 
     public Exercise createExerciseForExerciseSlot(Exercise exercise, Long exerciseSlotId, String username){
+
         ExerciseSlot exerciseSlot = exerciseSlotRepository.getById(exerciseSlotId);
-        exercise.setExerciseSlot(exerciseSlot);
+
+        // Determine if there is an exercise already created, if not continue with the request, else throw an exception
+        if (exerciseSlot.getExercise() == null) {
+            exercise.setExerciseSlot(exerciseSlot);
+        } else {
+            throw new DuplicateExerciseCreationException("Exercise slot " + exerciseSlot.getId() + " already has an exercise. Please create a new slot");
+        }
 
         return exerciseRepository.save(exercise);
 
