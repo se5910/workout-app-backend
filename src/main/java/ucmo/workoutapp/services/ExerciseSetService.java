4e31f771337b928d72b67c1f18546b4dca827432
Joshua Haunty <jkh87440@ucmo.edu>
2020-05-05 13:57:52 -0500

    Fixed createOrUpdateExerciseForSlot SHOULD BE ... ExercisSlot* -> Next fix

--- src/main/java/ucmo/workoutapp/services/ExerciseSetService.java
+++ src/main/java/ucmo/workoutapp/services/ExerciseSetService.java
@@ -36,12 +36,12 @@ public class ExerciseSetService {
             throw new EntityNotFoundException("Week is null");
         }
 
-        if (!request.isCoach() && week.getExerciseSlot().getTemplate().getExercisePlan().getClient().getUser().equals(request)) {
-            throw new ClientNotFoundException("You are not a coach and you are not this client. Unable to make changes to 'Set'.");
+        if (!request.isCoach()) {
+            throw new CoachNotFoundException("You are no a coach you cannot create or update a week");
         }
 
-        if (request.isCoach() && week.getExerciseSlot().getTemplate().getExercisePlan().getClient().getCoach().equals(request.getUsername())) {
-            throw new ClientNotFoundException("You are not the coach of this client. Unable to make changes to 'Set'.");
+        if (request.isCoach() && !week.getExerciseSlot().getTemplate().getExercisePlan().getClient().getCoach().equals(request.getUsername())) {
+            throw new ClientNotFoundException("You are not a coach and you are not this client. Unable to make changes to 'Set'.");
         }
 
         if (exerciseSet.getId() == null) {
