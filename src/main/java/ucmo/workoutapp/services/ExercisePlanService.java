a1b1f1415d46780d146e994098d13d6e2162ac95
Joshua Haunty <jkh87440@ucmo.edu>
2020-05-05 19:57:57 -0500

    Updated comments for ExercisePlanService

--- src/main/java/ucmo/workoutapp/services/ExercisePlanService.java
+++ src/main/java/ucmo/workoutapp/services/ExercisePlanService.java
@@ -24,34 +24,46 @@ public class ExercisePlanService {
     private UserRepository userRepository;
 
     public ExercisePlan createOrUpdateExercisePlan(Long clientId, ExercisePlan exercisePlan, String username){
+        // Get a client object via the ID passed in.
         Client client = clientRepository.getById(clientId);
+
+        // Gather the user making the request by looking for its username.
         User request = userRepository.findByUsername(username);
 
+        // If the exercisePlan being sent from the API call is null, throw an error.
         if (exercisePlan == null) {
             throw new EntityNotFoundException("Exercise Plan not found");
         }
 
+        // If the client could not be found by ID, throw an error.
         if (client == null) {
             throw new ClientNotFoundException("Client not found");
         }
 
+        // If the user requesting this information is NOT a coach, and the requesting user (who is also a Client; see Client Entity for details)
+        // ... IS NOT the same client as the client data being collected, throw an error.
         if (!request.isCoach() || !client.getCoach().equals(request.getUsername())) {
             throw new CoachNotFoundException("You are not the coach of this client or you are not a coach at all.");
         }
 
+        // If an ExercisePlan already exists, Update it with passed in information from exercisePlan
         if (exercisePlan.getPlanId() != null) {
             ExercisePlan existingPlan = exercisePlanRepository.getByPlanId(exercisePlan.getPlanId());
 
+            // Set data that was passed in from header
             existingPlan.setName(exercisePlan.getName());
             existingPlan.setClient(exercisePlan.getClient());
             existingPlan.setTemplates(exercisePlan.getTemplates());
             existingPlan.setPlanId(exercisePlan.getPlanId());
 
+            // Save the object in the database
             return exercisePlanRepository.save(existingPlan);
         }
 
+        // Logic if not previously created, set the client (to associate it in the DB).
         exercisePlan.setClient(client);
 
+        // Save the object in the database
         return exercisePlanRepository.save(exercisePlan);
     }
 
