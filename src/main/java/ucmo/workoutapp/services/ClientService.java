87f3f9813ec580cbee82c67699f78752fe5e1045
Joshua Haunty <jkh87440@ucmo.edu>
2020-05-11 15:54:29 -0500

    Revert "Client refactoring"

--- src/main/java/ucmo/workoutapp/services/ClientService.java
+++ src/main/java/ucmo/workoutapp/services/ClientService.java
@@ -22,12 +22,10 @@ public class ClientService {
         User request = userRepository.findByUsername(username);
         Client client = clientRepository.getByUser(request);
 
-        if (request.isCoach()) {
-            throw new ClientNotFoundException("You are a coach you cannot change a client's information.");
-        }
-
-        if (clientObject.getId() != null) {
-            // POSSIBLE REFACTORING. LOOK INTO COACH RESPONSIBILITY
+        if (client != null) {
+            if (request.isCoach()) {
+                throw new ItemNotFoundException("You are a coach you cannot edit a clients profile.");
+            }
 
             client.setAge(clientObject.getAge());
             client.setBodyFatPercentage(clientObject.getBodyFatPercentage());
